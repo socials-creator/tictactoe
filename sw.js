93f136c4f2114c1f0f05df586c380614b01d1d5f
socials-creator <me@nihar.ca>
2026-09-04 16:37:47 -0400

    Refactor service worker for tic-tac-toe caching

--- sw.js
+++ sw.js
@@ -1,7 +1,109 @@
-const CACHE='ttt-ios-v1';
-const ASSETS=['./','./index.html','./style.css','./script.js','./manifest.webmanifest','./icon.svg'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{
- const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res;
-}).catch(()=>caches.match('./index.html')))));
+const CACHE_NAME =
+  "tic-tac-toe-5x5-v2";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.webmanifest",
+  "./icon.svg"
+];
+
+
+self.addEventListener(
+  "install",
+  event => {
+
+    event.waitUntil(
+
+      caches
+        .open(CACHE_NAME)
+        .then(cache =>
+          cache.addAll(ASSETS)
+        )
+        .then(() =>
+          self.skipWaiting()
+        )
+
+    );
+  }
+);
+
+
+self.addEventListener(
+  "activate",
+  event => {
+
+    event.waitUntil(
+
+      caches
+        .keys()
+        .then(keys =>
+          Promise.all(
+
+            keys
+              .filter(
+                key =>
+                  key !== CACHE_NAME
+              )
+              .map(key =>
+                caches.delete(key)
+              )
+
+          )
+        )
+        .then(() =>
+          self.clients.claim()
+        )
+
+    );
+  }
+);
+
+
+self.addEventListener(
+  "fetch",
+  event => {
+
+    event.respondWith(
+
+      caches
+        .match(event.request)
+        .then(cached => {
+
+          if (cached) {
+            return cached;
+          }
+
+          return fetch(
+            event.request
+          )
+            .then(response => {
+
+              const copy =
+                response.clone();
+
+              caches
+                .open(CACHE_NAME)
+                .then(cache =>
+                  cache.put(
+                    event.request,
+                    copy
+                  )
+                );
+
+              return response;
+
+            })
+            .catch(() =>
+              caches.match(
+                "./index.html"
+              )
+            );
+
+        })
+
+    );
+  }
+);
